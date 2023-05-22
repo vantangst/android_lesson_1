65ef9001a3cc3a53d2b373238248a964133aa512
tangle <tang.le@liv3ly.io>
2023-05-22 21:26:15 +0700

    add left menu

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.helloworld
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -9,8 +11,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.helloworld.model.CalculatorResult
+import com.google.android.material.navigation.NavigationView
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
@@ -41,6 +46,10 @@ class MainActivity : AppCompatActivity() {
         Log.e("MainActivity", "Second activity callback: " + data?.getStringExtra("second_key_1"))
     }
 
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
+
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putSerializable("key_result", listResult)
         super.onSaveInstanceState(outState)
@@ -58,9 +67,39 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
         // custom overflow icon
 //        toolbar.overflowIcon = getDrawable(R.drawable.ic_baseline_settings_24)
+        drawer = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.leftMenu)
+        navigationView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_setting -> {
+                    goToSecondActivity()
+                }
+                else -> {
+
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+
+        //setup listener for drawer and actionbar
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        // show drawer menu toggle button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
         setSupportActionBar(toolbar)
         setupUI()
 
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -69,6 +108,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
         when (item.itemId) {
             R.id.action_history -> {
                 goToSecondActivity()
